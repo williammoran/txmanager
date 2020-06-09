850fb84bd0885b763abb909355093212340034fe
Bill Moran <wmoran@potentialtech.com>
2020-06-09 19:01:24 -0400

    switch back to standard logger till I have some more time to sort this out

--- core/tracker.go
+++ core/tracker.go
@@ -4,9 +4,8 @@ import "log"
 
 // Tracker exists to make tracking down errors easier
 type Tracker struct {
-	File   string
-	Line   int
-	Logger log.Logger
+	File string
+	Line int
 }
 
 // Finalize is a NOOP
@@ -20,7 +19,7 @@ func (t *Tracker) Commit() {}
 // Abort reports the file/line the transaction was created
 // on to make tracking down errors easier
 func (t *Tracker) Abort() {
-	t.Logger.Printf(
+	log.Printf(
 		"Aborting trasaction started %s:%d",
 		t.File, t.Line,
 	)
